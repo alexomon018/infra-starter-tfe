c52a21d743374d8c33585276c23afe7023e019ae
Aleksa <aleksa.mitic5859@gmail.com>
2024-10-31 19:11:43 +0100

    revert

--- locals.tf
+++ locals.tf
@@ -11,6 +11,11 @@ locals {
       project_id     = module.project["example-project"].id
       identifier     = "${var.github_organization_name}/infra-starter-tfe"
     }
-
+    "infra-github" = {
+      description    = "Example automation workspaces for github resources"
+      execution_mode = "local" // there is no state first time so it is set to local
+      project_id     = module.project["example-project"].id
+      identifier     = "${var.github_organization_name}/infra-starter-github"
+    }
   }
 }
