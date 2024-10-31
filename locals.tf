e26b62de6b58d7ec181f323dee2891205c1e29e8
Aleksa <a.mitic@Aleksas-Mac-mini.local>
2024-10-31 10:01:19 +0100

    feat: setup github workspace

--- locals.tf
+++ locals.tf
@@ -9,7 +9,13 @@ locals {
             description = "Example description for workspace"
             execution_mode = "remote" 
             project_id = module.project["example-project"].id
-            identifier = "alexomon018/infra-starter-tfe"
+            identifier = "${var.github_organization_name}/infra-starter-tfe"
+        }
+        "infra-github" = {
+            description = "Example automation workspaces for github resources"
+            execution_mode = "local" // there is no state first time so it is set to local
+            project_id = module.project["example-project"].id
+            identifier = "${var.github_organization_name}/infra-starter-github"
         }
     }
 }
